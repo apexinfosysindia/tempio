c6bd1b9186b4d97dd49b0c7f6e3e8704693437a7
ApexOS build-pipeline <build-pipeline@apexos.invalid>
2026-07-04 16:20:02 +0530

    ApexOS 0.1.1-dev: tempio from upstream 5420e16c8 (build-pipeline 221ca3abd)

---
+++ README.md
@@ -0,0 +1,42 @@
+# tempio
+A template helper for docker images which using [text/template](https://golang.org/pkg/text/template/) golang engine with [sprig functions](http://masterminds.github.io/sprig/).
+
+Support conf format:
+
+- json
+
+## Basic Example
+
+```bash
+tempio \
+    -conf /config/coredns.json \
+    -template /usr/share/tempio/corefile \
+    -out /etc/corefile
+```
+
+## Pipe Example
+
+```bash
+echo '{"some": "value"}' | tempio \
+    -template /usr/share/tempio/corefile \
+    -out /etc/corefile
+```
+
+## No Outfile Example
+
+_When  `-out` is not supplied, the result is printed to the console._
+
+```bash
+$ echo '{"some": "value"}' | tempio \
+    -template /usr/share/tempio/corefile
+> {"some": "value", "other": "value"}
+```
+
+## Add tempio to your GitHub actions
+
+```yaml
+- name: Setup tempio
+  uses: apexinfosysindia/tempio@main
+- name: Run tempio
+  run: tempio -help
+```
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+var TempioVersion string
+
+func main() {
+	var config *map[string]interface{}
+	configFile := flag.String("conf", "", "Config json file, can be omitted if used in a pipe")
+	templateFile := flag.String("template", "", "Template file")
+	outFile := flag.String("out", "", "Output file, if not defined output will be to console")
+
+	flag.Usage = func() {
+		writer := flag.CommandLine.Output()
+
+		fmt.Fprintf(writer, "Version: %s\n", TempioVersion)
+		fmt.Fprintf(writer, "Documentation: https://github.com/apexinfosysindia/tempio\n\n")
+
+		flag.PrintDefaults()
+	}
+
+	// parse command lines
+	flag.Parse()
+	if *templateFile == "" {
+		log.Fatal("Missing template argument")
+	}
+
+	// Get config
+	config = readConfig(*configFile)
+
+	// create & write corefile
+	data := renderTemplateFile(config, *templateFile)
+	if *outFile == "" {
+		fmt.Println(string(data))
+	} else {
+		err := os.WriteFile(*outFile, data, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
